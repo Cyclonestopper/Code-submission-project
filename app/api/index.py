871b99529b9a614f99a3c496d6e0e6996be81c85
Cyclonestopper <166185162+Cyclonestopper@users.noreply.github.com>
2024-11-22 08:48:21 -0800

    Added permissions

--- app/api/index.py
+++ app/api/index.py
@@ -97,8 +97,8 @@ def generate_script(file_path):
 
     script_content = f"""#!/bin/bash
 echo "Starting compilation of {file_path}"
-
-g++ -std=c++17 -o "/tmp/compiled_program" "{file_path}" 2> /tmp/compile_errors.log
+chmod +x /tmp/compiled_program
+g++ -o "/tmp/compiled_program" "{file_path}" 2> /tmp/compile_errors.log
 
 if [ $? -ne 0 ]; then
     echo "Compilation failed. Errors:"
