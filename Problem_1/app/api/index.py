0bd051066fcdfac72c2ac421d2a00726cee357f2
Cyclonestopper <166185162+Cyclonestopper@users.noreply.github.com>
2024-11-28 14:40:11 -0800

    method

--- Problem_1/app/api/index.py
+++ Problem_1/app/api/index.py
@@ -188,10 +188,10 @@ def run_script(script_name, file_path):
         # Printing both stdout and stderr
         print("STDOUT:", result.stdout)
         print("STDERR:", result.stderr)
-        
+        a={file_path}
         returncode = result.returncode
         if returncode == 2:
-            return {"success": True, "error": "Compilation failed, at file path "{file_path}"", "verdict": "Compile error"}
+            return {"success": True, "error": "Compilation failed, at file path "+a, "verdict": "Compile error"}
         elif returncode==1:
             return {"success":False, "error":"Internal error","verdict":"Internal error"}
         elif returncode==3:
